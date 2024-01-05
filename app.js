a59136ef9bb391b4d8c4281ea0235c522622aa24
luchofci <luciano.federico.c@gmail.com>
2024-01-05 08:33:29 -0300

    Cambios en catch por error

--- app.js
+++ app.js
@@ -3,6 +3,11 @@ const app = express();
 
 const userRoutes = require('./routes/user.routes');
 
+//Middlewares
+app.use(express.json());
+
+
+
 app.use(userRoutes)
 
 
--- controllers/user.controllers.js
+++ controllers/user.controllers.js
@@ -1,13 +1,21 @@
 const User = require('../models/user.model')
 
-async function getUser(req, res){
-    try{
+async function getUser(req, res) {
+    try {
         const users = await User.find()
 
-        res.send(users)
+        res.send({
+            users,
+            message: 'Usuarios obtenidos correctamente',
+            ok: true
+        })
     }
-    catch(error){
-        console.log(error)
+    catch (error) {
+        console.log(error);
+        res.send({
+            message: 'Error al obtener usuarios',
+            ok: false
+        })
     }
 }
 
@@ -15,21 +23,36 @@ async function getUser(req, res){
 
 
 
-function hellowController (req, res){
+function hellowController(req, res) {
 
     res.send('Hola mundo desde User Controller')
 }
 
-function createUser(req, res){
-    res.send('POST nuevo usuario')
+
+
+function createUser(req, res) {
+
+    const user = req.body;
+
+    console.log(users)
+
+    res.send("Post user")
+
+
+
 }
 
 
-function deleteUser (req, res){
+
+
+
+
+
+function deleteUser(req, res) {
     res.send('DELETE usuario')
 }
 
-function updateUser (req, res){
+function updateUser(req, res) {
     res.send('UPDATE usuario')
 }
 
