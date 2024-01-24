f0a637be24cf26aec1caa1afd39ac79d2ab5db88
luchofci <luciano.federico.c@gmail.com>
2024-01-24 13:05:20 -0300

    Varios

--- app.js
+++ app.js
@@ -6,12 +6,12 @@ const userRoutes = require('./routes/user.routes');
 const productRoutes = require('./routes/product.routes')
 
 //Middlewares -  Esto es importante para recordarle a la app que sabe leer Json (cuando se realice la request, el servidor tenga la habilidad de entender el body que estoy recibiendo.-)
-app.use(express.json());
+app.use(express.json()); //Cuando venga un re.body poder leerlo
 app.use(express.urlencoded({extended:true}));
 app.use(cors());
 
 // Aplicamos o integramos las rutas a nuestros server
-app.use([userRoutes, productRoutes])
+app.use([userRoutes, productRoutes]) //Esto es para que la app use las rutas definidas en userRoutes
 
 module.exports = app;
 
