abdffb3a8a1fdf10908d9cf67f70b9e79dc1f4e0
luchofci <luciano.federico.c@gmail.com>
2024-02-19 12:06:42 -0300

    Add uploadProductImage

--- app.js
+++ app.js
@@ -8,8 +8,12 @@ const categoryRoutes=require("./routes/category.routes")
 //Middlewares(aplicaciones intermedias) acciones que se ejecutan en mi servidor antes de llamara a cualquier ruta
 app.use(express.json());//cuando venga un req.body poder leerlo
 app.use(cors()); //incorporar servivio cors a las funcionalidades
+
+app.use(express.static('public')) // Compartir carpeta Public, para poder ver las fotos guardadas en el front.-
+
 app.use(express.urlencoded({ extended: true }));
 //Aplicamos o Integramos las rutas a nuestro server
+
 app.use([userRoutes,productRoutes, categoryRoutes]); //que app use las rutas definidas en userRoutes/productRoutes
 
 module.exports = app;
---
+++ middlewares/uploadProductImage.js
@@ -0,0 +1,19 @@
+const multer =  require('multer');
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) =>{
+        cb(null, 'public/images/products')
+    },
+    filename: (req, file, cb) =>{
+        cb(null, `${Date.now()}-${file.originalname}`)
+    }
+})
+
+const uploadMulter = multer({
+    storage: storage,
+
+})
+
+const upload = uploadMulter.single('image'); // Campo de la request donde viene el archivo ('lo q esta en parentesis')
+
+module.exports = upload;
